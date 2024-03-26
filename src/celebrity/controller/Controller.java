3ca59aef7ec44f0e3d70753f3488c40dca56e725
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-03-26 08:08:14 -0600

    First commit

---
+++ src/celebrity/controller/Controller.java
@@ -0,0 +1,14 @@
+package celebrity.controller;
+
+public class Controller
+{
+	public Controller()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+	}
+}
---
+++ src/celebrity/controller/Runner.java
@@ -0,0 +1,10 @@
+package celebrity.controller;
+
+public class Runner
+{
+	public static void main(String[] args)
+	{
+		Controller app = new Controller();
+		app.start();
+	}
+}
